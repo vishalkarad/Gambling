e566766d8cabc520bd751a807130f06508e0c310
Your Name <you@example.com>
2020-02-28 17:10:55 +0530

    Added : Calculate avery day you win or lost and print total amount

--- Gambling.sh
+++ Gambling.sh
@@ -7,24 +7,36 @@ echo "*********************Gambling*************************"
 STAKE=100
 BET=1
 IS_WIN=1
-
+cash=$STAKE
 function playGame()
 {
 	result=$((RANDOM%2))
 	#Check win or loss
 
-	if [[ $result -eq IS_WIN ]]
+	if [[ $result -eq $IS_WIN ]]
 	then 
-		STAKE=$(( $STAKE + $BET ))
+		cash=$(( $cash + $BET ))
+		echo "1"
 	else
-		STAKE=$(( $STAKE - $BET ))
+		cash=$(( $cash - $BET ))
+		echo "0"
 	fi
 }
 playGame
 # Win or lost 50 %
-while [[ $STAKE -lt 150 && $STAKE -gt 50 ]]
+while [[ $cash -lt 150 && $cash -gt 50 ]]
 do 
-	echo "$STAKE"
 	playGame
 done
 
+# print win or loose after 20 days playing
+for (( index=1; index<=20; index++ ))
+do 
+	bet=$( playGame )
+	if [ $bet -eq $IS_WIN ]
+	then	
+		printf "you win 1 And your total stack is : $cash"
+	else
+		printf "you loose 1 And your total stack is : $cash"
+	fi
+done
